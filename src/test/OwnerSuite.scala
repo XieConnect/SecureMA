cb75da4bc774771fc8a8a73025c2427acbb94f2e
Wei Xie <XieConnect@gmail.com>
2012-09-25 01:16:21 -0500

    Verify data preparation correctness

---
+++ src/test/OwnerSuite.scala
@@ -0,0 +1,34 @@
+package test
+
+/**
+ * @description Test Data Owners
+ * @author Wei Xie <wei.xie (at) vanderbilt.edu>
+ * @version 9/24/12
+ */
+
+import org.scalatest.FunSuite
+import main.{Helpers, Owner}
+import java.io.File
+
+class OwnerSuite extends FunSuite {
+  val rawDataFile = Helpers.property("raw_data_file")
+
+  test("file containing raw data is not empty") {
+    expect(true) (new File(rawDataFile).exists())
+    assert(io.Source.fromFile(rawDataFile).getLines().drop(1).size > 0)
+  }
+
+  test("prepareData() outputs non-empty result") {
+    val encryptedFile = Helpers.property("encrypted_data_file")
+    Owner.prepareData(rawDataFile, encryptedFile)
+    val encryptedLines = io.Source.fromFile(encryptedFile).getLines().toArray
+    assert(encryptedLines.size > 1)
+    expect(Owner.MULTIPLIER) (encryptedLines(0).split(",")(1).toDouble)
+  }
+
+  test("verify correctness of encrypted result") {
+    val encryptedFile = Helpers.property("encrypted_data_file")
+    expect(true) (Owner.verifyEncryption(encryptedFile))
+  }
+
+}
