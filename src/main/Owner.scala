22c75fb9a42ca84f5ca69595b442752d6b9a985c
Wei Xie <XieConnect@gmail.com>
2012-09-25 01:16:21 -0500

    Verify data preparation correctness

--- src/main/Owner.scala
+++ src/main/Owner.scala
@@ -58,7 +58,42 @@ object Owner {
     println(">> Encrypted data saved in: " + encryptedFile)
   }
 
+  /**
+   * Verify correctness of encrypted data before submitting to others
+   * @param encryptedFile file containing encryptions
+   * @return true if no errors found
+   */
+  def verifyEncryption(encryptedFile: String = Helpers.property("encrypted_data_file")): Boolean = {
+    val lines = io.Source.fromFile(encryptedFile).getLines().toArray
+    var multiplierCorrect = true
+    var rowsCorrect = true
+
+    if (lines(0).split(",")(1).toDouble != MULTIPLIER)
+      multiplierCorrect = false
+
+    for ((line, indx) <- lines.drop(2).view.zipWithIndex if multiplierCorrect == true; record = line.split(",")) {
+      //verify weight_i
+      if (Mediator.decryptData(new BigInteger(record(0))) != Helpers.toBigInteger(record(3).toDouble * MULTIPLIER)) {
+        println("  weight_i error in row " + indx)
+        rowsCorrect = false
+      }
+
+      if (Mediator.decryptData(new BigInteger(record(1))) != new BigInteger(record(5))) {
+        println("  positive beta*weight error in row " + indx)
+        rowsCorrect = false
+      }
+
+      if (Mediator.decryptData(new BigInteger(record(2))) != new BigInteger(record(6))) {
+        println("  negative beta*weight error in row " + indx)
+        rowsCorrect = false
+      }
+    }
+
+    multiplierCorrect && rowsCorrect
+  }
+
   def main(args: Array[String]) = {
     prepareData()
+    //verifyEncryption()
   }
 }
