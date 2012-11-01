b5eb2a080736117c6fbfd6a49824f1e2ac1661e1
Wei Xie <XieConnect@gmail.com>
2012-11-01 01:29:35 -0500

    Task dispatch related scripts

--- prepare_experiment.sh
+++ prepare_experiment.sh
@@ -6,14 +6,15 @@
 # Note:
 #   - no slash in experiment_name
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-  echo "./prepare_experiment.sh <experiment_name> <number_of_processes>"
+  echo "./prepare_experiment.sh <experiment_name>"
   exit
 fi
 
 basedir=experiment/$1
 mkdir -p $basedir
+cp copy_processes.sh $basedir/
 
 expdir=$basedir/process0/
 mkdir -p $expdir
@@ -23,14 +24,3 @@ cp conf.properties $expdir
 cp -r run $expdir
 cp -r start $expdir
 mkdir -p $expdir"data"
-
-# generate multiple copies if needed
-if [ $2 -gt 1 ]
-then
-    endIndx=$2
-    let endIndx=endIndx-1
-    for indx in $(eval echo {1..$endIndx})
-    do
-        cp -r $expdir $basedir/process"$indx"
-    done
-fi
