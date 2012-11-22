c14b028d5878d74543f99315403bef02aa57f7fd
Wei Xie <XieConnect@gmail.com>
2012-11-21 18:21:28 -0600

    Plot results from Eagle T2D data

--- plot_accuracy.r
+++ plot_accuracy.r
@@ -1,24 +1,24 @@
 #!/usr/bin/Rscript
 
 # to customize
-datadir <- "experiment/meta_SNP_phenotype_ethnicity/data/"
+datadir <- "~/data/eagle_T2D_without_combined/"
 
-mydata <- read.csv(paste(datadir, "final_result.csv", sep=""), skip=2)
-experiment_indices=15
+mydata <- read.csv(paste(datadir, "final_result.csv", sep=""), skip=1)
+experiment_indices=34
 
 # plot accuracy
 png(paste(datadir, "accuracy.png", sep=""))
 ## absolute error
-plot(mydata[, 9],
+plot(mydata[, 3],
      type="l",
-     xlim=c(1, experiment_indices),
-     main="accuracy of secure meta-analysis",
-     xlab="experiment index",
+     #xlim=c(1, experiment_indices),
+     main="error of secure meta-analysis",
+     xlab="index of experiment",
      ylab="error of secure meta-analysis",
      col="blue")
 
 ## relative error
-lines(mydata[, 10],
+lines(mydata[, 4],
       col="red")
 
 legend("topright",
@@ -31,12 +31,12 @@ legend("topright",
 
 
 ## plot runtime (over all experiments)
-png(paste(datadir, "runtime_for_all_experiments.png", sep=""))
-plot((mydata[, 11] + mydata[, 12])/1000,
+png(paste(datadir, "runtime_SMC_and_division.png", sep=""))
+plot((mydata[, 8] + mydata[, 7]),
      col="blue",
-     xlim=c(1, experiment_indices),
-     main="runtime with varying participating sites",
-     xlab="experiment index",
+     #xlim=c(1, experiment_indices),
+     main="runtime of different experiments",
+     xlab="index of experiment",
      ylab="runtime (seconds)",
      type="l")
 
@@ -44,20 +44,22 @@ plot((mydata[, 11] + mydata[, 12])/1000,
 ## plot runtime breakdown
 mytime <- read.csv(paste(datadir, "division_time_breakdown.csv", sep=""), skip=2)
 
-png(paste(datadir, "lnx_runtime_breakdown_for_numerator.png", sep=""))
+png(paste(datadir, "runtime_lnx_breakdown_for_numerator.png", sep=""))
 barplot(t(data.frame(mytime[, 2:3])),
         col=c("blue", "red"),
-        legend=c("Fairplay", "poly. eval."),
+        legend.text=c("Fairplay", "poly. eval."),
+        args.legend=list(x="center"),
         main="ln(x) runtime breakdown for numerator"
         )
 
-png(paste(datadir, "lnx_runtime_breakdown_for_denominator", sep=""))
+
+png(paste(datadir, "runtime_lnx_breakdown_for_denominator.png", sep=""))
 barplot(t(data.frame(mytime[, 4:5])),
         col=c("blue", "red"),
-        legend=c("Fairplay", "poly. eval."),
+        legend.text=c("Fairplay", "poly. eval."),
+        args.legend=list(x="center"),
         main="ln(x) runtime breakdown for denominator"
         )
-
 
 # close output
 dev.off()
