443b868268c2c2832ee1720426d0fe5ca998ee50
Wei Xie <XieConnect@gmail.com>
2014-02-07 14:58:21 -0600

    Misc files for running experiments

--- scripts/circuit.r
+++ scripts/circuit.r
@@ -1,25 +1,26 @@
 #!/usr/bin/Rscript
 # simulate the binary circuit for estimating n and epsilon
+
 args <- commandArgs(TRUE)
 
+# Input two shares for X
 x1 <- as.integer(args[1])
 x2 <- as.integer(args[2])
 
-x <- x1 + x2
-
 # to customize inputs
 maxN <- 80
-bitLength <- 128
+bitLength <- 80
+# input randomization parameters
+randa <- 0
+randb <- 0
  
-randa <- 11
-randb <- 22
-
 ## init
 est <- 1
 n <- 0
+x <- x1 + x2
+print(paste("Input x:  ", x))
 
-print(paste("x:  ", x))
-
+# Estimate n and est
 for (i in 1:maxN) {
   if (est < x) {
     est = 2*est
@@ -27,9 +28,8 @@ for (i in 1:maxN) {
   }
 }
 
-print(paste("est:  ", est))
-print(paste("n:    ", n))
-
+print(paste("[Rough estimate] est:  ", est))
+print(paste("[Rough estimate] n:    ", n))
 
 ## scale epsilon
 jEnd <- maxN - n
@@ -42,13 +42,14 @@ for (i in 1:maxN) {
 
 }
 
-#print(paste("est:  ", est))
+print(paste("Scaled epsilon:  ", est))
 
 print("-- Server: ---")
 print(paste("est - randa:  ", est - randa))
+print(paste("   or (est + fieldSize):  ", est - randa + 2^bitLength))
 print(paste("n - randb:  ", n - randb))
 
 print("")
 print("-- Client: ---")
-print(paste("- randa:  ", - randa))
-print(paste("- randb:  ", - randb))
+print(paste("- randa:  ", randa))
+print(paste("- randb:  ", randb))
